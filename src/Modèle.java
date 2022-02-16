a23bef3d841919241fd9facb51af00b1c30f20da
Solène <solene0privat@gmail.com>
2022-02-16 11:47:32 +0100

    déclaration des attributs du modèle 1

--- src/Modèle.java
+++ src/Modèle.java
@@ -2,7 +2,7 @@ import java.awt.Color;
 
 public class Modèle {
 
-	public Color[] COLOR= {"Jaune","Vert","Bleu","Magenta","Rouge","Orange","Blanc","Noir"};
+	public Color[] COLOR= {Color.yellow,Color.green,Color.blue,Color.magenta,Color.red,Color.orange,Color.white,Color.black};
 	public int N_TENTIVES;
 	public int DIFFICULTE;
 	//Alt+0201
