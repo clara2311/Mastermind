6ae60c70049b40536f0a87fecab1b051fd8ea725
Solène <solene0privat@gmail.com>
2022-02-16 11:59:23 +0100

    modele1

--- src/Modèle.java
+++ src/Modèle.java
@@ -4,7 +4,7 @@ public class Modèle {
 
 	public Color[] COLOR= {Color.yellow,Color.green,Color.blue,Color.magenta,Color.red,Color.orange,Color.white,Color.black};
 	public int N_TENTIVES;
-	public int DIFFICULTE;
+	public static int DIFFICULTE;
 	//Alt+0201
 	public enum Etat {EN_COURS,GAGNÉ,PERDU};
 	
