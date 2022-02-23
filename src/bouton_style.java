797df12bf12385a8228851130216dacb23a399c8
Clara <clara.garnier80@gmail.com>
2022-02-23 10:44:08 +0100

    gestions des boutons

---
+++ src/bouton_style.java
@@ -0,0 +1,15 @@
+import java.awt.Button;
+import java.awt.Color;
+
+public class bouton_style extends Button {
+
+	public bouton_style(Color color) {
+		this.setBackground(color);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/boutons_couleurs.java
+++ src/boutons_couleurs.java
@@ -1,8 +1,29 @@
+import java.awt.Button;
+import java.awt.Color;
 
-public class boutons_couleurs {
+import javax.swing.JPanel;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class boutons_couleurs extends JPanel {
+	public boutons_couleurs() {
+		bouton_style b1=new bouton_style(Color.yellow);
+		this.add(b1);
+		bouton_style b2=new bouton_style(Color.green);
+		this.add(b2);
+		bouton_style b3=new bouton_style(Color.blue);
+		this.add(b3);
+		bouton_style b4=new bouton_style(Color.pink);
+		this.add(b4);
+		bouton_style b5=new bouton_style(Color.red);
+		this.add(b5);
+		bouton_style b6=new bouton_style(Color.yellow);
+		this.add(b6);
+		bouton_style b7=new bouton_style(Color.white);
+		this.add(b7);
+		bouton_style b8=new bouton_style(Color.black);
+		this.add(b8);
+		
+		
+
 
 	}
 
