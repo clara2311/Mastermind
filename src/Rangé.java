cd1a97a6449d062983a8cd5b8306dc383995dfac
Solène <solene0privat@gmail.com>
2022-02-23 10:40:37 +0100

    avancer classe rangé

--- src/Rangé.java
+++ src/Rangé.java
@@ -1,4 +1,15 @@
+import java.awt.Color;
 
 public class Rangé {
-
+	
+	public Color[] jeton;
+	public int indiceJeton;
+	public int[] coupleNoirBlanc;
+	
+	public Rangé() {
+		jeton=new Color[Modèle.DIFFICULTE];
+		indiceJeton=0;
+		coupleNoirBlanc=new int[2];
+	}
+	
 }
